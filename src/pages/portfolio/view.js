65c7b75184d7735a9675c6d39a69ff1a26ebb394
George Savchenko <george.savchenko@gmail.com>
2019-04-18 01:27:55 -0400

    converting to typescript

--- src/pages/portfolio/view.js
+++ src/pages/portfolio/view.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import { Link } from 'gatsby'
 
-import Layout from './../../components/Layout'
+import Layout from '../../components/Layout'
 
 export class view extends Component {
   render() {
