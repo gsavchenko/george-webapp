827f9c766573ff645c235564f51c4b0dab42d987
George Savchenko <george.savchenko@gmail.com>
2019-05-04 22:39:32 -0400

    adding postcss to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,12 @@ module.exports = {
   pathPrefix: '/',
   plugins: [
     {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [require(`postcss-preset-env`)({ stage: 0 })],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
