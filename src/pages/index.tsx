ee1fddff01023de41d0742120246f9a629a35d79
gsavchenko <gsavchenko@users.noreply.github.com>
2020-06-27 16:14:23 -0400

    moving logic to fix incorrect height on mobile (#6)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,12 @@ import { Home } from '../app/+home/components';
 import { ShellContainer } from '../app/core/shared/+shell';
 
 class SiteIndex extends React.Component {
-  constructor(props: {}) {
-    super(props);
-
-    this.setAppHeight = this.setAppHeight.bind(this);
-  }
-
   render() {
     return (
       <ShellContainer>
         <Home />
       </ShellContainer>
     );
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.setAppHeight);
-    this.setAppHeight();
-  }
-
-  private setAppHeight(): void {
-    const doc = document.documentElement;
-    doc.style.setProperty('--app-height', `${window.innerHeight}px`);
   }
 }
 
