ed50fb04cd19d7df19810a35ba57d032bfd93951
George Savchenko <george.savchenko@affinity.co>
2021-09-12 16:03:28 -0400

    getting rid of ugly comma dangles

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,30 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   plugins: [
     '@typescript-eslint',
-    'react',
+    'react'
   ],
   globals: {
-    graphql: false,
+    graphql: false
   },
   parserOptions: {
     sourceType: 'module',
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   extends: [
     'airbnb',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   rules: {
     'no-anonymous-exports-page-templates': 'warn',
     'limited-exports-page-templates': 'warn',
-    'comma-dangle': 'never'
-  },
+    'comma-dangle': ['error', 'never']
+  }
 };
