39e9c3394953690a739705898fc1ba68b6541eab
George Savchenko <george.savchenko@affinity.co>
2021-09-12 15:55:59 -0400

    WIP - ts linting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
+const path = require('path')
+// Get paths of Gatsby's required rules, which as of writing is located at:
+// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
+// gatsby/src/utils/eslint-rules
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  'node_modules',
+  'gatsby',
+  'dist',
+  'utils',
+  'eslint-rules'
+)
+
 module.exports = {
   siteMetadata: {
     title: 'george-webapp',
@@ -8,34 +21,34 @@ module.exports = {
   pathPrefix: '/',
   plugins: [
     {
-      resolve: `gatsby-plugin-typescript`,
+      resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: false, // defaults to false
-        jsxPragma: `React`, // defaults to "React"
+        jsxPragma: 'React', // defaults to "React"
         allExtensions: false, // defaults to false
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
         name: 'blog',
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
           'gatsby-plugin-typescript-checker',
@@ -45,17 +58,17 @@ module.exports = {
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-feed`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-feed',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: { icons: [] },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-react-redux`,
+      resolve: 'gatsby-plugin-react-redux',
       options: {
         // [required] - path to module you created in step 1
         pathToCreateStoreModule: './src/app/store',
@@ -76,8 +89,21 @@ module.exports = {
         configFile: './stylelint.config.js',
       },
     },
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-ramda`,
-    `gatsby-plugin-postcss`,
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be ommitted or customized
+        stages: ['develop'],
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        exclude: ['node_modules', 'bower_components', '.cache', 'public'],
+        // Any additional eslint-webpack-plugin options below
+        // ...
+      },
+    },
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-ramda',
+    'gatsby-plugin-postcss',
   ],
 }
