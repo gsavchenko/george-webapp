66ff9313fca390651a4e9f06fcd317b119f6c1b0
George Savchenko <george.savchenko@affinity.co>
2021-10-02 13:11:08 -0400

    commenting out blog index for now

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -7,37 +7,30 @@ interface IBlogIndexProps {
   location: string;
 }
 
-class BlogIndex extends React.Component<IBlogIndexProps, {}> {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
-    const siteDescription = get(
-      this,
-      'props.data.site.siteMetadata.description',
-    );
-    const posts = get(this, 'props.data.allMarkdownRemark.edges');
+const BlogIndex:React.FC<IBlogIndexProps> = () => {
+  const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+  const siteDescription = get(this, 'props.data.site.siteMetadata.description');
+  // const posts = get(this, 'props.data.allMarkdownRemark.edges');
 
-    return (
-      <div title={siteTitle}>
-        <Helmet
-          htmlAttributes={{ lang: 'en' }}
-          meta={[{ name: 'description', content: siteDescription }]}
-          title={`Blog | ${siteTitle}`}
-        />
-        <h2>Blog</h2>
-          const title = get(node, 'frontmatter.title') || node.frontmatter.path;
-          return (
-            <div key={posts[0].node.frontmatter.path}>
-              <h3>
-                <Link to={posts[0].node.frontmatter.path}>
-                  {posts[0].node.frontmatter.title}
-                </Link>
-              </h3>
-              <small>{posts[0].node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: posts[0].node.excerpt }} />
-            </div>
-      </div>
-    );
-  }
+  return (
+    <div title={siteTitle}>
+      <Helmet
+        htmlAttributes={{ lang: 'en' }}
+        meta={[{ name: 'description', content: siteDescription }]}
+        title={`Blog | ${siteTitle}`}
+      />
+      <h2>Blog</h2>
+      {/* <div key={posts[0].node.frontmatter.path}>
+        <h3>
+          <Link to={posts[0].node.frontmatter.path}>
+            {posts[0].node.frontmatter.title}
+          </Link>
+        </h3>
+        <small>{posts[0].node.frontmatter.date}</small>
+        <p dangerouslySetInnerHTML={{ __html: posts[0].node.excerpt }} />
+      </div> */}
+    </div>
+  )
 }
 
 export default BlogIndex;
