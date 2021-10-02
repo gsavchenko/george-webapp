7dc8d84052436af4b3f496c75ea9bc167ee0b64f
George Savchenko <george.savchenko@affinity.co>
2021-10-02 12:58:03 -0400

    cleaning up contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,27 +3,23 @@ import get from 'lodash/get';
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import contactData from '../data/contact';
-
-class Contact extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
-    const siteDescription = get(
-      this,
-      'props.data.site.siteMetadata.description',
-    );
-    return (
-      <div>
-        <Helmet
-          htmlAttributes={{ lang: 'en' }}
-          meta={[{ name: 'description', content: siteDescription }]}
-          title={`Contact | ${siteTitle}`}
-        />
-        <h2>Contact</h2>
-        <br />
-      </div>
-    );
-  }
+const Contact: React.FC = () => {
+  const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+  const siteDescription = get(
+    this,
+    'props.data.site.siteMetadata.description'
+  );
+  return (
+    <div>
+      <Helmet
+        htmlAttributes={{ lang: 'en' }}
+        meta={[{ name: 'description', content: siteDescription }]}
+        title={`Contact | ${siteTitle}`}
+      />
+      <h2>Contact</h2>
+      <br />
+    </div>
+  );
 }
 
 export default Contact;
