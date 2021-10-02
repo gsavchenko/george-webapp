1703e599c09b5bf2342bd7aaa3174e4493eb60c2
George Savchenko <george.savchenko@affinity.co>
2021-10-02 13:24:46 -0400

    fixing up journey

--- src/pages/journey.tsx
+++ src/pages/journey.tsx
@@ -5,49 +5,43 @@ import Helmet from 'react-helmet';
 import { FaAward } from 'react-icons/fa';
 import {
   VerticalTimeline,
-  VerticalTimelineElement,
+  VerticalTimelineElement
 } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import LightboxImage from '../common/components/LightboxImage';
 
-class Journey extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
-    const siteDescription = get(
-      this,
-      'props.data.site.siteMetadata.description',
-    );
+const Journey:React.FC = () => {
+  const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+  const siteDescription = get(this, 'props.data.site.siteMetadata.description');
 
-    return (
+  return (
+    <div>
+      <Helmet
+        htmlAttributes={{ lang: 'en' }}
+        meta={[{ name: 'description', content: siteDescription }]}
+        title={`Journey | ${siteTitle}`}
+      />
+
+      <h2>Journey</h2>
+
       <div>
-        <Helmet
-          htmlAttributes={{ lang: 'en' }}
-          meta={[{ name: 'description', content: siteDescription }]}
-          title={`Journey | ${siteTitle}`}
-        />
-
-        <h2>Journey</h2>
-
-        <div>
-          <VerticalTimeline animate={false}>
-              <VerticalTimelineElement
-                className='vertical-timeline-element--work'
-                date={'some date'}
-                iconStyle={{ background: '#fff', color: '#34495e' }}
-                icon={<FaAward />}
-                key={'headline'}
-              >
-                <h3 className='vertical-timeline-element-title'>
-                  {'headline'}
-                </h3>
-                <br />
-                {'description'}
-              </VerticalTimelineElement>
-          </VerticalTimeline>
-        </div>
+        <VerticalTimeline animate={false}>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="some date"
+            iconStyle={{ background: '#fff', color: '#34495e' }}
+            icon={<FaAward />}
+            key="headline"
+          >
+            <h3 className="vertical-timeline-element-title">
+              headline
+            </h3>
+            <br />
+            description
+          </VerticalTimelineElement>
+        </VerticalTimeline>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Journey;
