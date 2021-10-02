02326299e6abb30d9be7fc54e81782e75b78aedf
George Savchenko <george.savchenko@affinity.co>
2021-10-02 13:44:59 -0400

    fixing up 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,26 +4,27 @@ interface INotFoundPageProps {
   location: string;
 }
 
-class NotFoundPage extends React.Component<INotFoundPageProps, {}> {
-  render() {
-    return (
-      <div>
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist....</p>
-        <div style={{width: '100%', height: 0, paddingBottom: '70%', position: 'relative'}}>
-        <iframe
-          src='https://giphy.com/embed/OK27wINdQS5YQ'
-          width='100%'
-          height='100%'
-          style={{position: 'absolute'}}
-          frameBorder={0}
-          className='giphy-embed'
-          allowFullScreen={true}>
-        </iframe>
-        </div><p><a href='https://giphy.com/gifs/mind-seinfeild-OK27wINdQS5YQ'>via GIPHY</a></p>
-      </div>
-    );
-  }
-}
+const NotFoundPage:React.FC<INotFoundPageProps> = () => (
+  <div>
+    <h1>Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist....</p>
+    <div style={{
+      width: '100%', height: 0, paddingBottom: '70%', position: 'relative'
+    }}
+    >
+      <iframe
+        title="seinfeld"
+        src="https://giphy.com/embed/OK27wINdQS5YQ"
+        width="100%"
+        height="100%"
+        style={{ position: 'absolute' }}
+        frameBorder={0}
+        className="giphy-embed"
+        allowFullScreen
+      />
+    </div>
+    <p><a href="https://giphy.com/gifs/mind-seinfeild-OK27wINdQS5YQ">via GIPHY</a></p>
+  </div>
+);
 
 export default NotFoundPage;
