4cc07dc321a7069c7a6e0d266b5b1f54df477418
George Savchenko <george.savchenko@affinity.co>
2021-10-16 15:53:45 -0400

    cleaning up shell container

--- src/features/shell/containers/shell.container.tsx
+++ src/features/shell/containers/shell.container.tsx
@@ -14,25 +14,28 @@ const mapStateToProps = (state: AppState): ShellState => ({
   menuToggled: state.menu.toggled
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<Menu.Actions>): ShellDispatch => {
-  return {
-    openMenu: () => dispatch(Menu.open()),
-    closeMenu: () => dispatch(Menu.close())
-  };
-};
+const mapDispatchToProps = (dispatch: Dispatch<Menu.Actions>): ShellDispatch => ({
+  openMenu: () => dispatch(Menu.open()),
+  closeMenu: () => dispatch(Menu.close())
+});
 
-class ShellContainer extends React.Component<ShellProps, ShellState> {
-  render() {
-    return (
-      <Shell
-        openMenu={this.props.openMenu}
-        closeMenu={this.props.closeMenu}
-        menuToggled={this.props.menuToggled}>
-        {this.props.children}
-      </Shell>
-    );
-  }
+const ShellContainer = (props: ShellProps) => {
+  const {
+    openMenu,
+    closeMenu,
+    menuToggled,
+    children
+  } = props;
+
+  return (
+    <Shell
+      openMenu={openMenu}
+      closeMenu={closeMenu}
+      menuToggled={menuToggled}
+    >
+      {children}
+    </Shell>
+  );
 }
 
-export default connect<ShellState, {}>
-(mapStateToProps, mapDispatchToProps)(ShellContainer);
+export default connect<ShellState>(mapStateToProps, mapDispatchToProps)(ShellContainer);
