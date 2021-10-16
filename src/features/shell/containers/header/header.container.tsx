b959f18c7b71fa986b7dd9304df6b3d8ccee669f
George Savchenko <george.savchenko@affinity.co>
2021-10-16 16:39:31 -0400

    cleaning up header container

--- src/features/shell/containers/header/header.container.tsx
+++ src/features/shell/containers/header/header.container.tsx
@@ -14,23 +14,25 @@ const mapStateToProps = (state: AppState): HeaderState => ({
   menuToggled: state.menu.toggled
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<Menu.Actions>): HeaderDispatch => {
-  return {
-    openMenu: () => dispatch(Menu.open()),
-    closeMenu: () => dispatch(Menu.close())
-  };
-};
+const mapDispatchToProps = (dispatch: Dispatch<Menu.Actions>): HeaderDispatch => ({
+  openMenu: () => dispatch(Menu.open()),
+  closeMenu: () => dispatch(Menu.close())
+});
 
-class HeaderContainer extends React.Component<HeaderProps, HeaderState> {
-  render() {
-    return (
-      <Header
-        openMenu={this.props.openMenu}
-        closeMenu={this.props.closeMenu}
-        menuToggled={this.props.menuToggled}/>
-    );
-  }
+const HeaderContainer = (props: HeaderProps) => {
+  const {
+    openMenu,
+    closeMenu,
+    menuToggled
+  } = props;
+
+  return (
+    <Header
+      openMenu={openMenu}
+      closeMenu={closeMenu}
+      menuToggled={menuToggled}
+    />
+  );
 }
 
-export default connect<HeaderState, HeaderProps>
-(mapStateToProps, mapDispatchToProps)(HeaderContainer);
+export default connect<HeaderState, HeaderProps>(mapStateToProps, mapDispatchToProps)(HeaderContainer);
