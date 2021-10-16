6ef71163b24f500de40c968b66e85456ca102dc0
George Savchenko <george.savchenko@affinity.co>
2021-10-16 13:56:03 -0400

    refactoring site index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Home } from '../features/home';
 import { ShellContainer } from '../features/shell';
 
-class SiteIndex extends React.Component {
-  render() {
-    return (
-      <ShellContainer>
-        <Home />
-      </ShellContainer>
-    );
-  }
-}
+const SiteIndex = (): ReactNode => (
+  <ShellContainer>
+    <Home />
+  </ShellContainer>
+);
 
 export default SiteIndex;
