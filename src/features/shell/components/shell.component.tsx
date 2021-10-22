db252c198331a0230c313add84c9e05ff6e2d986
George Savchenko <george.savchenko@affinity.co>
2021-10-22 16:48:34 -0400

    cleaning up shell component

--- src/features/shell/components/shell.component.tsx
+++ src/features/shell/components/shell.component.tsx
@@ -1,24 +1,24 @@
-import { HeaderContainer } from '../containers';
-import { ShellProps } from '../store';
-import { Footer } from './footer';
 import React from 'react';
 import classnames from 'classnames/bind';
+import HeaderContainer from '../containers/header/header.container';
+import { ShellProps } from '../store';
+import { Footer } from './footer';
 import * as styles from './shell.component.css';
 
 const cx = classnames.bind(styles);
 
-class Shell extends React.Component<ShellProps, {}> {
-  render() {
-    return (
-      <div className={cx('container', { 'sidenav-open' : this.props.menuToggled })}>
-        <HeaderContainer></HeaderContainer>
-        <div className={cx('body', { 'sidenav-open' : this.props.menuToggled })}>
-          {this.props.children}
-        </div>
-        <Footer></Footer>
+const Shell: React.FC<ShellProps> = (props) => {
+  const { menuToggled, menuToggled, children } = props;
+
+  return (
+    <div className={cx('container', { 'sidenav-open': menuToggled })}>
+      <HeaderContainer />
+      <div className={cx('body', { 'sidenav-open': menuToggled })}>
+        {children}
       </div>
-    );
-  }
+      <Footer />
+    </div>
+  );
 }
 
 export default Shell;
