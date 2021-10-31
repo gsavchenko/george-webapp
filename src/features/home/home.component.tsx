5a3972d707e780655df4abded0d6a6ca5e22693d
George Savchenko <george.savchenko@affinity.co>
2021-10-31 13:06:51 -0400

    cleaning up home component

--- src/features/home/home.component.tsx
+++ src/features/home/home.component.tsx
@@ -1,32 +1,32 @@
-import { AnimatedDescription } from './animated-description';
 import { graphql, useStaticQuery } from 'gatsby';
 import classnames from 'classnames/bind';
 import get from 'lodash/get';
 import React from 'react';
 import Hemlet from 'react-helmet';
-import * as styles from './home.component.css';
+
+import { AnimatedDescription } from './animated-description';
 import { AnimatedBackground } from '../../common/components/animated-background';
+
+import * as styles from './home.component.css';
 
 classnames.bind(styles);
 
-class Home extends React.Component {
-  render() {
-    const siteTitle = 'George Savchenko';
-    const siteDescription = get(this, 'props.data.site.siteMetadata.description');
+const Home: React.FC = () => {
+  const siteTitle = 'George Savchenko';
+  const siteDescription = get(this, 'props.data.site.siteMetadata.description');
 
-    return (
-      <div>
-        <Hemlet title={siteTitle} meta={[{name: 'description', content: siteDescription, }]}/>
-        <AnimatedDescription />
-        <AnimatedBackground />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Hemlet title={siteTitle} meta={[{ name: 'description', content: siteDescription }]} />
+      <AnimatedDescription />
+      <AnimatedBackground />
+    </div>
+  );
 }
 
 export default Home;
 
-export const query = () => useStaticQuery(graphql`
+export const query = (): void => useStaticQuery(graphql`
   query {
     site {
       siteMetadata {
