2c7c3b8e286115c98ddf5ca06ea6c63f56231c78
George Savchenko <george.savchenko@affinity.co>
2021-10-31 14:26:14 -0400

    temp fix for shell component

--- src/features/shell/components/shell.component.tsx
+++ src/features/shell/components/shell.component.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import classnames from 'classnames/bind';
+
 import HeaderContainer from '../containers/header/header.container';
 import { ShellProps } from '../store';
-import { Footer } from './footer';
+import Footer from './footer/footer.component';
+
 import * as styles from './shell.component.css';
 
 const cx = classnames.bind(styles);
 
 const Shell: React.FC<ShellProps> = (props) => {
-  const { menuToggled, menuToggled, children } = props;
+  const { children } = props;
 
+  // TODO: fix side menu open
   return (
-    <div className={cx('container', { 'sidenav-open': menuToggled })}>
+    // <div className={cx('container', { 'sidenav-open': menuToggled })}>
+    <div className={cx('container')}>
       <HeaderContainer />
-      <div className={cx('body', { 'sidenav-open': menuToggled })}>
+      {/* <div className={cx('body', { 'sidenav-open': menuToggled })}> */}
+      <div className={cx('body')}>
         {children}
       </div>
       <Footer />
