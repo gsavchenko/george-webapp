9d7c0905327b23b1289d5f55ce390536ed54fa94
George Savchenko <george.savchenko@affinity.co>
2021-10-31 14:06:36 -0400

    cleaning up vertical layout component

--- src/common/components/vertical-layout/vertical-layout.tsx
+++ src/common/components/vertical-layout/vertical-layout.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import VerticalLayoutProps from './vertical-layout.interface';
-import * as styles from './vertical-layout.css';
+import React, { ReactNode } from 'react';
 import classnames from 'classnames';
+
+import * as styles from './vertical-layout.css';
+
+interface VerticalLayoutProps {
+  top: ReactNode;
+  bottom: ReactNode;
+}
 
 const cx = classnames.bind(styles);
 
-class VerticalLayout extends React.Component<VerticalLayoutProps, {}> {
-  render() {
-    return (
-      <div className={cx('stacklayout-container')}>
-        <div className={cx('top')}>{this.props.top}</div>
-        <div>{this.props.bottom}</div>
-      </div>
-    );
-  }
+const VerticalLayout: React.FC<VerticalLayoutProps> = (props) => {
+  const { top, bottom } = props;
+
+  return (
+    <div className={cx('stacklayout-container')}>
+      <div className={cx('top')}>{top}</div>
+      <div>{bottom}</div>
+    </div>
+  );
 }
 
 export default VerticalLayout;
