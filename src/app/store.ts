3547c5b3b22695683e231cb427adb143cb5ca4fa
George Savchenko <george.savchenko@affinity.co>
2021-11-21 16:33:41 -0500

    cleaning up store file

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
-import { rootReducer } from './root.reducer';
+import {
+  createStore,
+  DeepPartial,
+  StateFromReducersMapObject,
+  Store
+} from 'redux'
+import { rootReducer } from './root.reducer'
+
+export type RootState = StateFromReducersMapObject<typeof rootReducer>
 
 // preloadedState will be passed to you by the plugin
-export default (preloadedState) => {
-  return createStore(rootReducer, preloadedState);
-};
+export default (preloadedState: DeepPartial<RootState>): Store => createStore(rootReducer, preloadedState)
