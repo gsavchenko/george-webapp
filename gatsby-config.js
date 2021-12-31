f70f7b0bf5d5b2565bc380c7e2c7d285bc78964a
George Savchenko <george.savchenko@affinity.co>
2021-12-31 01:05:34 -0500

    Merge branch 'fix/add-eslint' into fix/typescript-linting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
-const path = require('path');
+const path = require("path");
 // Get paths of Gatsby's required rules, which as of writing is located at:
 // https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
 // gatsby/src/utils/eslint-rules
 const gatsbyRequiredRules = path.join(
   process.cwd(),
-  'node_modules',
-  'gatsby',
-  'dist',
-  'utils',
-  'eslint-rules'
+  "node_modules",
+  "gatsby",
+  "dist",
+  "utils",
+  "eslint-rules"
 );
 
 module.exports = {
@@ -16,59 +16,72 @@ module.exports = {
     title: 'george-webapp',
     author: 'George Savchenko',
     description: 'Get your George Savchenko digest here!',
-    siteUrl: 'http://georgesavchenko.com'
+    siteUrl: 'http://georgesavchenko.com',
   },
   pathPrefix: '/',
   plugins: [
     {
-      resolve: 'gatsby-plugin-typescript',
+      resolve: `gatsby-plugin-typescript`,
       options: {
-        isTSX: false, // defaults to false
+        isTSX: true, // defaults to false
         jsxPragma: 'React', // defaults to "React"
-        allExtensions: false // defaults to false
-      }
+        allExtensions: true, // defaults to false
+      },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'blog'
-      }
+        name: 'blog',
+      },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590
-            }
+              maxWidth: 590,
+            },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem'
-            }
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
           },
           'gatsby-plugin-typescript-checker',
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants'
-        ]
-      }
+          'gatsby-remark-smartypants',
+        ],
+      },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-feed',
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be ommitted or customized
+        stages: ["develop"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        // Any additional eslint-webpack-plugin options below
+        // ...
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-feed`,
+    {
+      resolve:`gatsby-plugin-manifest`,
       options: { icons: [] }
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-plugin-react-redux',
+      resolve: `gatsby-plugin-react-redux`,
       options: {
         // [required] - path to module you created in step 1
         pathToCreateStoreModule: './src/app/store',
@@ -78,25 +91,20 @@ module.exports = {
         serialize: {
           space: 0,
           isJSON: true,
-          unsafe: false
-        }
-      }
+          unsafe: false,
+        },
+      },
     },
     {
-      resolve: 'gatsby-plugin-eslint',
+      resolve: "@danbruegge/gatsby-plugin-stylelint",
       options: {
-        // Gatsby required rules directory
-        rulePaths: [gatsbyRequiredRules],
-        // Default settings that may be ommitted or customized
-        stages: ['develop'],
-        extensions: ['js', 'jsx', 'ts', 'tsx'],
-        exclude: ['node_modules', 'bower_components', '.cache', 'public']
-        // Any additional eslint-webpack-plugin options below
-        // ...
+        files: ["**/*.{ts,tsx}"],
+        configFile: './stylelint.config.js'
       }
     },
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-ramda',
-    'gatsby-plugin-postcss'
-  ]
-};
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-ramda`,
+    `gatsby-plugin-postcss`
+  ],
+}
+    
