31307ec1a3f39e2cd921b82c398d26c16fc5045d
George Savchenko <george.savchenko@affinity.co>
2022-01-29 17:31:42 -0500

    cleaning up redux wrapper

--- src/app/redux-wrapper.tsx
+++ src/app/redux-wrapper.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './store';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { rootReducer } from './root-reducer'
 
 const reduxWrapper = ({ element }) => (
-  <Provider store={createStore(rootReducer)}>
-    {element}
-  </Provider>
-);
+  <Provider store={createStore(rootReducer)}>{element}</Provider>
+)
 
-export default reduxWrapper;
+export default reduxWrapper
