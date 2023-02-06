602c39e92675be066542499cd135e4afdf3df9a0
George Savchenko <george.savchenko@gmail.com>
2023-02-05 23:34:59 -0500

    deleting travis ci stuff

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SSH_USER=$SSH_USER
-SSH_HOST=138.197.155.168
-SSH_SRC=$TRAVIS_BUILD_DIR/public/
-SSH_DST=/home/projects/george-webapp/public
-
-if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "master" ]; then
-  rsync -azP $SSH_SRC $SSH_USER@$SSH_HOST:$SSH_DST
-fi
