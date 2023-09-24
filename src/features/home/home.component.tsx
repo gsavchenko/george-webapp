c6d3c3e07e1ced0806f1bcb40de87ba32fbec1a0
George Savchenko <george.savchenko@gmail.com>
2023-09-24 00:39:44 -0400

    update imports

--- src/features/home/home.component.tsx
+++ src/features/home/home.component.tsx
@@ -6,8 +6,7 @@ import { OrbitControls } from '@react-three/drei';
 import { Nodes } from '../../components/three/nodes';
 import styled from '@emotion/styled';
 import { MdMenu, MdClose } from 'react-icons/md';
-import { useSidebar, Sidebar } from '../../common/components';
-import { Logo } from '../../common/components/logo';
+import { useSidebar, Sidebar, Logo } from '../../components/common';
 import { IconButton } from '../icon';
 
 const CanvasBackground = styled.div((props) => ({
