36b2a06410d676439f83c60ea8f2eaac983e11ba
George Savchenko <george.savchenko@gmail.com>
2024-03-19 15:48:39 -0400

    refactor - move global providers to gatsby-browser

--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-export function onInitialClientRender() {
-  require('typeface-merriweather');
-  require('typeface-source-sans-pro');
-}
---
+++ gatsby-browser.tsx
@@ -0,0 +1,23 @@
+import { ThemeProvider, Global } from '@emotion/react';
+import { Theme } from '@radix-ui/themes';
+import { GatsbyBrowser } from 'gatsby';
+import { theme } from './src/styles/theme';
+
+export function onInitialClientRender() {
+  require('typeface-merriweather');
+  require('typeface-source-sans-pro');
+}
+
+export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
+  element,
+}) => {
+  return (
+    <Theme>
+      <ThemeProvider theme={theme}>
+        {/* Removes a default 8px margin that break 100vh */}
+        <Global styles={{ body: { margin: '0px' } }} />
+        {element}
+      </ThemeProvider>
+    </Theme>
+  );
+};
