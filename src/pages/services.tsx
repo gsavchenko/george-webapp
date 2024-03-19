f30c591ea3075b08f8c4924c30fc167ecbff1caa
George Savchenko <george.savchenko@gmail.com>
2024-03-19 16:55:24 -0400

    add services page

---
+++ src/pages/services.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { MainLayout } from '../modules/layouts';
+import { Pricing } from '../modules/services/pricing.component';
+
+const SectionContainer = styled.div({
+  paddingTop: '20px',
+  textAlign: 'center',
+});
+
+const Services = (): JSX.Element => {
+  return (
+    <MainLayout>
+      <MainLayout.Header>
+        <SectionContainer>
+          <h1>Services</h1>
+        </SectionContainer>
+      </MainLayout.Header>
+      <MainLayout.Body>
+        <Pricing />
+        <SectionContainer>
+          <p>
+            Discover the Unmatched Personalization of a Custom-Built Website —
+            No Cookie-Cutter Templates. Just Unique, Tailored Design and
+            Flexible Growth Plans That Fit Your Business Like a Glove.
+          </p>
+        </SectionContainer>
+      </MainLayout.Body>
+      <MainLayout.Footer></MainLayout.Footer>
+    </MainLayout>
+  );
+};
+
+export default Services;
